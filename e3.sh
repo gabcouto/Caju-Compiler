4e2164b47e5538804fa936d5a209c072a269cfea
gabcouto <gabrieliervolinof@gmail.com>
2023-02-19 19:34:24 -0300

    Adicionei o testador e os testes.

---
+++ e3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -f temp.dot
+rm -f temp1.dot
+./etapa3 < tests/$1 | sed 's/, 0x/ -> 0x/g' | sed 's/0x/x/g' > temp.dot
+(echo "digraph {" && echo "label=\"Ref\"" && cat temp.dot && echo "}") > temp1.dot
+rm -f temp.dot
+mv temp1.dot temp.dot
+xdot temp.dot &
